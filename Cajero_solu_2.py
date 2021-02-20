b9f462b09ab31df78b9941bc4b0464bb0b237a61
Edwin Roman <romanseberino@gmail.com>
2021-02-19 22:44:45 -0400

    Cerca de la solucion pero aun falta, hasta aqui puedo decir los valores que se necesitan

--- Cajero_solu_2.py
+++ Cajero_solu_2.py
@@ -1,20 +1,34 @@
 papeletas = [100,200,500,1000]
-monto = 2000
+monto = int(input('Ingrese su monto a retirar: '))
 
 devueltas = []
 
-for papeleta in papeletas:
-    #print(monto//b)
-    if(monto/papeleta == 1):
-        print('Usted puede pagar con 1 {}'.format(papeleta))
-        break
+def Proceso(monto):
+    for papeleta in papeletas:
+        #print(monto//b)
+        if(monto/papeleta == 1):
+            print('Usted puede pagar con 1 billete de {}'.format(papeleta))
+            exit()
 
-    if((monto//papeleta) != 0):
-        devueltas.append([monto//papeleta,papeleta])
+        if((monto//papeleta) != 0):
+            devueltas.append([monto//papeleta,papeleta])
+    
+    devueltas_finales = sorted(devueltas)
+    #Con esto se puede lograr pagar con las papeletas mayores, lo cual  puedo dar las de mayor valor que seria menos
+    #fisicamente que es lo que se logra o persive buscar. (LOGICO MUCHO, FISICO POCO.)
 
-devueltas_finales = sorted(devueltas)
-#print(devueltas_finales)
+    #print(devueltas_finales)
 
-for dev in devueltas_finales:
-    print('Usted tiene que dar ',dev[0],' billetes de ',dev[1])
-    #if(monto < 0):
+    for dev in devueltas_finales:
+        print('Usted tiene que dar ',dev[0],' billetes de ',dev[1])
+        monto = monto - (dev[0]*dev[1])
+        #break (Para comprobar el monto o la valides de la operacion)
+        if(monto > 0):
+            Proceso(monto)
+        else:
+            exit()
+            
+    print('MOnto total: {}'.format(monto))
+
+
+Proceso(monto)
